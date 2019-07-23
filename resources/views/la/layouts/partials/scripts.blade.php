4c7e82cf4222bc2791a99c6d8ef9a7a220544ef8
TunTunMin <33777403+TunTunMin@users.noreply.github.com>
2019-07-23 22:32:36 +0630

    'summernote-edit'

--- resources/views/la/layouts/partials/scripts.blade.php
+++ resources/views/la/layouts/partials/scripts.blade.php
@@ -1,7 +1,7 @@
 <!-- REQUIRED JS SCRIPTS -->
 
 <!-- jQuery 2.1.4 -->
-<script src="{{ asset('la-assets/plugins/jQuery/jQuery-2.1.4.min.js') }}"></script>
+<script src="{{ asset('js/jquery-1.11.1.js') }}"></script>
 <!-- Bootstrap 3.3.2 JS -->
 <script src="{{ asset('la-assets/js/bootstrap.min.js') }}" type="text/javascript"></script>
 
